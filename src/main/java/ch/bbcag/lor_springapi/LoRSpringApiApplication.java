b51aaf28a135feb1e85a7d8b052a50acf70de08a
Dreier Nicolas <nicolas.dreier@bbcag.ch>
2022-02-03 09:00:21 +0100

    changed project to Springboot. a bit messy

---
+++ src/main/java/ch/bbcag/lor_springapi/LoRSpringApiApplication.java
@@ -0,0 +1,13 @@
+package ch.bbcag.lor_springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoRSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoRSpringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbcag/lor_springapi/LoRSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbcag.lor_springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoRSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
